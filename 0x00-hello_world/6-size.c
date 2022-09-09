69cfaa9b6e0c20008da07db807b1b9be01b14332
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-09 04:04:18 +0100

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long int li;
-	long long int lli;
+	long li;
+	long long lli;
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f))
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
