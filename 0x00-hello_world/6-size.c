181bf42dc713406b20681c1aa64c691ae85a4fcf
Chidinmajessy <chidinmajessy16@gmail.com>
2022-09-08 19:41:38 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+/**
+* main - print the string in the input function
+*
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long int b;
+	long long int d;
+	float e;
+
+	printf("size of a char: %d byte(s)\n", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(i));
+	printf("size of a long int: %byte(s)\n", sizeof(b));
+	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	return (0);
+}
