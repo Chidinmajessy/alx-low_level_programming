84d3ffdbd94b0d872774798de640c749f410b111
Chidinmajessy <chidinmajessy16@gmail.com>
2022-09-09 21:52:49 -0700

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main – Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i =0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
+
+	return (0);
+}
+
