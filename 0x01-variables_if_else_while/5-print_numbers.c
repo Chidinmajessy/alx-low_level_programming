f05562302f2c74d27d3a8ba6be2c4bca7d55af3c
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-10 14:51:12 +0100

    single digit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,10 +3,8 @@
 #include <stdio.h>
 
 /**
- * main – print if the number is positive,zero,or negative
+ * main - print if the number is positive, zero or negative
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
