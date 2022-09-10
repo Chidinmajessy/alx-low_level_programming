848e12810604c6f721b5726c4be4294b85fe008a
Chidinmajessy <chidinmajessy16@gmail.com>
2022-09-09 21:38:45 -0700

    lower case only

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - print if the number is positive, zero or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low !=e && low != q)
+			putchar(low);
+	}
+		printf("\n");
+
+	return (0);
+}
+
