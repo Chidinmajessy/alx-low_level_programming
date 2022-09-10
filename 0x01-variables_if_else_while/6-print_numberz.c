823976487bd466136101314a5c140f58a15a4846
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-10 14:57:31 +0100

    lower case

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 
 /**
- * main – print entry point
+ * main - print if the number is positive, zero or negative
  *
  * Return: 0
  */
 
 int main(void)
 {
-	int d;
-	for (d = '0'; d <= '9'; d++)
-
+int d;
+for (d = '0'; d <= '9'; d++)
 {	
 	putchar(d);
 }
-		putchar('\n');
+	putchar('\n');
+
 return (0);
 }
 
