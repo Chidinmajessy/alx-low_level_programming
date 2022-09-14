f07daf5fe41597d1a426356c3364d7d01dc00688
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-14 07:53:26 +0100

    times table n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
+			for j = 0; j <= n; j++)
+			{
 			k = j * i;
 			if (j == 0)
 			{
@@ -44,6 +46,7 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
+}
 }
 
 
