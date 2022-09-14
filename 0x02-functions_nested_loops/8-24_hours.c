d21f5c78c65781fe345b94c05536371fbabfacbc
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-14 05:41:54 +0100

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 	i = 0;
 
-	while (i < 24)
+	while (i < 24))
 	{
 		j = 0;
 		while (j < 60)
@@ -19,7 +19,6 @@ void jack_bauer(void)
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
