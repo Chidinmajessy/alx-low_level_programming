56d806b95892746a662d6d8690d80a0bc8f89dd6
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-16 05:44:40 +0100

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,20 +11,20 @@ void print_square(int size)
 {
 	int inc1, inc2;
 
-	if (size > 0)
+	if (size < 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
-			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			for (inc2 = 0; inc2 < size; inc2++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-			putchar('#');
-			putchar('\n');
+			_putchar('#');
+			_putchar('\n');
 		}
 	}
 	else
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 }
