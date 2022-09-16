606103fcdfe3ab8929d2bac880d722619c2c32be
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-16 08:04:11 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,11 +19,11 @@ int main(void)
 		printf("Fizz ");
 	} else if (i % 5 == 0 && i % 3 !=0)
 	{
-		printf(" Buzz");
+		printf("Buzz ");
 	} 
 	else if (i % 3 == 0 && i % 5 == 0)
 	{
-		printf("FizzBuzz");
+		printf("FizzBuzz ");
 	} else if (i == 1)
 	{
 		printf("%d ", i);
