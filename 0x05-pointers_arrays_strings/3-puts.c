55f9d8c57531e2271633281a041c1d0e57e29a06
Chidinmajessy <chidinmajessy16@gmail.com>
2022-09-19 02:42:37 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#incluede "main.h"
+
+/**
+ * _puts - printsa string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_puchar(str[i]);
+	}
+	_putchar('\n');
+}
