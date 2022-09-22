8a488eef9c50b62be3c5521c9e6e937da1d0eb1f
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-22 07:00:00 +0100

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,7 +6,9 @@
  *Assume that strings are never empty and
  *numbers will always be positive or 0
  *Assumes there are only digits stored in the number strings
- *If result can be stored in the buffer, returns '0'
+ *If result can be stored in the buffer
+ *returns a pointer to the result
+ *If results cannot be stored in the buffer, returns '0'.
  *@n1: first number to be added
  *@n2: second num ber to be added
  *@r: store result
@@ -26,8 +28,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
  * @r_index: the current index of the buffer
  *
  * Return: if r can store the sum - a pointer to the result
- * 	   if r cannot store the sum - 0.
+ *		if r cannot store the sum - 0.
  */
+
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
@@ -40,12 +43,18 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 
-	for (; *n1; n1--; r-index++)
+	for (; *n1; n1--; r_index++)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens + num / 10;
 	}
 
-}
+	for (; *n2; n2--; r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
 
+}
