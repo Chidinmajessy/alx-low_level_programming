8f3942ecbe89944013896e9df6c532e60f427ce1
Chidinmajessy <chidinmajessy16@gmail.com>
2022-09-21 19:14:25 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * adds one line of code
+ * only one statement
+ * code should not modify p
+ * not allowed to use a
+ * only the required code should be written
+ *
+ */
+int main()
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+*(p + 5) = 98
+	/* ...so that this prints98\n*/
+	printf("a[2] = %d\n", a[2];
+			return (0);
+}
