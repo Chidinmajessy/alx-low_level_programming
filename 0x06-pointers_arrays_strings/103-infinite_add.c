99f1026ff3a20b23722bb444f7574765941fcbe5
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-22 07:20:19 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -39,7 +39,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	{
 		num = (*n1 - '0') * tens;
 		num += tens;
-		*(r * r_index) = (num % 10) + '0';
+		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
@@ -47,7 +47,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens + num / 10;
+		tens = num / 10;
 	}
 
 	for (; *n2; n2--; r_index--)
