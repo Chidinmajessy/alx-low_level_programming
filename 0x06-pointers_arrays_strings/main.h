b603071ed4777840cf6f2f7466511b88d0c3b39d
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-09-22 12:26:48 +0100

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char);
-char *leet(char *);
+char *leet(char *s);
 char *add_string(char *n1, char *n2, char *r, int r_index);
 
 #endif
